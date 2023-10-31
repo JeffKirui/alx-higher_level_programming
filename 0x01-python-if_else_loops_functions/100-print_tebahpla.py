0bc80208f47314ff41400622770c3870c6d68ee0
Jeff Kirui <kipkoech_g@yahoo.com>
2023-10-31 19:57:15 +0300

    Advanced Task 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 for letter in range(ord('z'), ord('a') - 1, - 1):
     if letter % 2 == 1:
         letter -= 32
-    print("{:s}".format(chr(letter)), end="")
+    print("{}".format(chr(letter)), end="")
