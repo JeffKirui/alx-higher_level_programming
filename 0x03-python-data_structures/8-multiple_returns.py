167713185ad2ae6def203aab355bc5358e98f842
Jeff Kirui <kipkoech_g@yahoo.com>
2023-11-06 15:53:18 +0300

    Task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    """Return tuple with string length and first character"""
+    length = len(sentence)
+    if length == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    return (length, first_char)
