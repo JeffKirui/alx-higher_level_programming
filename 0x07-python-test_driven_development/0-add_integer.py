d88074fe0ff758f25a692a0755925f0d1a89c9fe
Jeff Kirui <kipkoech_g@yahoo.com>
2023-11-26 21:08:17 +0300

    Fixing bug task 1

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,7 +17,7 @@ def add_integer(a, b=98):
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
-    if not isinstance(b, (it, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
