0b865cd06ec97fa7cfd625356eb1e0cb253827a6
Jeff Kirui <kipkoech_g@yahoo.com>
2023-12-05 06:32:51 -0800

    Fixing bug task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""
-Module of Student
-"""
+""" student module, creates a class student """
 
 
 class Student:
-    """
-    Student class
-    """
+    """ Class Student """
 
     def __init__(self, first_name, last_name, age):
-        """init function """
+        """ Initiliazes the instance """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """
-        to_json method of Student
-        """
-        return(self.__dict__)
+        """ Returns a json about the class """
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+        }
