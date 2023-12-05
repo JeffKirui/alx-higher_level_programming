ebed2012c74b481df9b4493555d6807a72596b8f
Jeff Kirui <kipkoech_g@yahoo.com>
2023-12-05 06:00:33 -0800

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-""" importing json """
-import json
-import os
-from sys import argv
+""" Adds items to a json file """
+import sys
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""
-Module of add items
-"""
-if __name__ == "__main__":
-    """ adding all the arguments passed by terminal """
-    name = "add_item.json"
+filename = "add_item.json"
+obj = [arg for arg in sys.argv if arg != sys.argv[0]]
 
-    if os.path.isfile(name):
-        my_list = load_from_json_file(name)
-    else:
-        my_list = []
-
-    for i in range(1, len(argv)):
-        my_list.append(argv[i])
-    save_to_json_file(my_list, name)
+try:
+    data = load_from_json_file(filename)
+except Exception:
+    save_to_json_file(obj, filename)
+else:
+    for o in obj:
+        data.append(o)
+    save_to_json_file(data, filename)
