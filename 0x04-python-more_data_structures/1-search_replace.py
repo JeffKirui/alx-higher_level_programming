b91c70227e3f99dc7ccc8d57be71c6e4b76c1067
Jeff Kirui <kipkoech_g@yahoo.com>
2023-12-31 10:13:05 +0300

    Rewrite code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """Replaces all occurrences of element by another in new list"""
-    new_list = my_list.copy()
-    new_list[:] = [x if x != search else replace for x in new_list]
+    new_list = []
+    for element in my_list:
+        if element == search:
+            element = replace
+        new_list.append(element)
     return new_list
