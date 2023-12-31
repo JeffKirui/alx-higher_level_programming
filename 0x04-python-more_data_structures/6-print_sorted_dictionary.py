5564c7c84bfb1b28bc2ae85559925a761813ab76
Jeff Kirui <kipkoech_g@yahoo.com>
2023-12-31 10:58:44 +0300

    Rewrite code

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     """Print dictionary by ordered keys."""
-    for i, s in sorted(a_dictionary.items(), key=lambda x: x[0]):
-        print("{}: {}".format(i, s))
+    [print("{}: {}".format(key, a_dictionary[key]))
+            for key in sorted(a_dictionary)]
