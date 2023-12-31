09ddaa146f612052b028eba347eaa1005ed80e9d
Jeff Kirui <kipkoech_g@yahoo.com>
2023-12-31 11:39:42 +0300

    Rewrite code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """Return key with biggest integer value."""
-    return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return (None)
+    
+    temp_key = list(a_dictionary.keys())[0]
+    temp_value = a_dictionary[temp_key]
+    for key, value in a_dictionary.items():
+        if value > temp_value:
+            temp_key = key
+            temp_value = value
+    return (temp_key)
