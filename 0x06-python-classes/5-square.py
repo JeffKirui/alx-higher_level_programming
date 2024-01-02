fdac7a785e37324bab5473f80097eff95ce8659c
Jeff Kirui <kipkoech_g@yahoo.com>
2024-01-02 16:57:57 +0300

    Rewrite code

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+""" Define a class Square. """
 
 
 class Square:
-    """ A class that defines a square by its size
-    """
+    """ A class that defines a square by its size. """
 
     def __init__(self, size=0):
-        """ Method to initialize the square object
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
-    def area(self):
-        """ Method that returns the square are of the object
-        """
-        return (self.__size ** 2)
+        """ Method to initialize the square object. """
+        self.__size = size
 
     @property
     def size(self):
-        """ Method to returns the size value
-        """
+        """ Method to returns the size value. """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Method to set the size value of the square object
-        """
+        """ Method to set the size value of the square object. """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -38,10 +24,12 @@ class Square:
         else:
             self.__size = value
 
+    def area(self):
+        """ Method that returns the square are of the object. """
+        return (self.__size ** 2)
+    
     def my_print(self):
-        """ Method that prints a # square according
-        to the size value
-        """
+        """ Method that prints a # square according to the size value. """
         if not self.__size:
             print()
         else:
